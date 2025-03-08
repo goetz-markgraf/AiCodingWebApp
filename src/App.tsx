528cc722dae8afe1b7e83388318e525a0d8bebaa
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2025-03-08 12:18:25 +0100

    create base project for further additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [location, setLocation] = useState('')
+  const [question, setQuestion] = useState('')
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>AI assisted coding</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>How is the weather today in …</p>
+        <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="card">
+        <p>I want to ask somthing:</p>
+        <input type="text" value={question} onChange={(e) => setQuestion(e.target.value)} />
+      </div>
     </>
   )
 }
